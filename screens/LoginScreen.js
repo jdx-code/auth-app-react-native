f396bb973614e68aea6316b0e8a50e97db8c8c29
Jyotirmoy <jyotirmoydas12@gmail.com>
2024-08-24 23:19:25 +0530

    Storing and managing the user authentication state with context, extracting the token

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,26 +1,24 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
 import { login } from '../util/auth';
 import { Alert } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function LoginScreen() {
 
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
+  const authCtx = useContext(AuthContext);
+
   async function loginHandler({email, password}) {
-    setIsAuthenticating(true);
-    
-    try {
-      
-      await login(email, password);
-
+    setIsAuthenticating(true);    
+    try {      
+      const token = await login(email, password);
+      authCtx.authenticate(token);
     } catch (error) {
-
       Alert.alert('Authentication Failed', 'Check your credentials or try again later.'); 
-           
-    }
-    
+    }    
     setIsAuthenticating(false);
   }
 
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,26 +1,23 @@
-import { useState } from 'react';
-import AuthContent from '../components/Auth/AuthContent';
+import { useContext, useState } from 'react';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
 import { createUser } from '../util/auth';
 import { Alert } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function SignupScreen() {
+  
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
+  const authCtx = useContext(AuthContext);
+
   async function signupHandler({email, password}) {
-    setIsAuthenticating(true);
-
+    setIsAuthenticating(true);    
     try{
-
-      await createUser(email, password);
-
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
     } catch (error) {
-
       Alert.alert('Sign up process failed', 'Check your input or try again later.');
-
     }
-
-    
     setIsAuthenticating(false);
   }
 
